d173df7e533b7ed4180594321f1cb159c744b759
Leonardo Lopes <leolopesribeiro05@gmail.com>
2024-01-02 23:39:16 -0300

    Arrumando problemas de estilo

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,8 +8,8 @@ export default function Home() {
   const storage = useStorage();
   const [modal, setModal] = useState(false);
   const [active, setActive] = useState("inactive");
-  const [task, setTask] = useState(JSON.parse(storage.takeTask())) // eslint-disable-line
-  
+  const [task, setTask] = useState(JSON.parse(storage.takeTask())); // eslint-disable-line
+
   return (
     <div className="container-home">
       <Modal style={modal ? `slide-right ${active}` : `slide-left ${active}`} />
@@ -22,18 +22,11 @@ export default function Home() {
         >
           <NewTask />
         </div>
-        {task != null ? (
-          task.map((data, id) => {
-
-            return (
-              <div key={id}>
-                <TaskCard data={data} />
-              </div>
-            );
-          })
-        ) : (
-          null
-        )}
+        {task != null
+          ? task.map((data, id) => {
+              return <TaskCard key={id} data={data} />;
+            })
+          : null}
       </div>
     </div>
   );
