6c68f6695d745bf71eceb2fd06f28881bfd57f5e
Leonardo Lopes <leolopesribeiro05@gmail.com>
2024-01-05 13:49:47 -0300

    Testando erro 6

--- src/pages/Lista/index.jsx
+++ src/pages/Lista/index.jsx
@@ -1,67 +1,67 @@
-import { useLocation } from "react-router-dom";
-import { useState } from "react";
-import { useStorage } from "../../hooks";
-import "./index.css";
+// import { useLocation } from "react-router-dom";
+// import { useState } from "react";
+// import { useStorage } from "../../hooks";
+// import "./index.css";
 
 export default function List() {
-  const storage = useStorage();
-  const { state } = useLocation();
-  //eslint-disable-next-line
-  const [task, setTask] = useState(storage.searchOneTask(state.data.nome));
-  const [newTask, setNewTasks] = useState([]);
-  const [taskExisted, setTaskExisted] = useState(task.tarefas);
-  const [inputEdit, setInputEdit] = useState(true);
+  // const storage = useStorage();
+  // const { state } = useLocation();
+  // //eslint-disable-next-line
+  // const [task, setTask] = useState(storage.searchOneTask(state.data.nome));
+  // const [newTask, setNewTasks] = useState([]);
+  // const [taskExisted, setTaskExisted] = useState(task.tarefas);
+  // const [inputEdit, setInputEdit] = useState(true);
 
-  let count = 0;
-  task.tarefas.forEach((dados) => {
-    if (dados.includes("%")) {
-      count++;
-    }
-  });
-  function handleChangeExisted(data, id) {
-    const inputValue = [...taskExisted];
-    inputValue[id] = data;
-    let value = inputValue;
-    setTaskExisted(value);
-  }
+  // let count = 0;
+  // task.tarefas.forEach((dados) => {
+  //   if (dados.includes("%")) {
+  //     count++;
+  //   }
+  // });
+  // function handleChangeExisted(data, id) {
+  //   const inputValue = [...taskExisted];
+  //   inputValue[id] = data;
+  //   let value = inputValue;
+  //   setTaskExisted(value);
+  // }
 
-  function edit() {
-    if (newTask.length > 0) {
-      const teste = taskExisted.concat(newTask);
-      console.log(teste);
-      storage.editTask(task, teste);
-    } else {
-      storage.editTask(task, taskExisted);
-    }
-  }
+  // function edit() {
+  //   if (newTask.length > 0) {
+  //     const teste = taskExisted.concat(newTask);
+  //     console.log(teste);
+  //     storage.editTask(task, teste);
+  //   } else {
+  //     storage.editTask(task, taskExisted);
+  //   }
+  // }
 
-  function tarefaCompleta(id) {
-    let tasksLocal = [...taskExisted];
-    if (tasksLocal[id] === `${task.tarefas[id]}%`) {
-      tasksLocal[id] = task.tarefas[id];
-      console.log("ei");
-    } else {
-      tasksLocal[id] = `${tasksLocal[id]}%`;
-    }
-    setTaskExisted(tasksLocal);
-  }
+  // function tarefaCompleta(id) {
+  //   let tasksLocal = [...taskExisted];
+  //   if (tasksLocal[id] === `${task.tarefas[id]}%`) {
+  //     tasksLocal[id] = task.tarefas[id];
+  //     console.log("ei");
+  //   } else {
+  //     tasksLocal[id] = `${tasksLocal[id]}%`;
+  //   }
+  //   setTaskExisted(tasksLocal);
+  // }
 
-  function handleChange(data, id) {
-    const inputValue = [...newTask];
-    inputValue[id] = data;
-    let value = inputValue;
-    setNewTasks(value);
-  }
-  function handleAdd() {
-    const newInput = [...newTask, []];
-    setNewTasks(newInput);
-  }
-  function handleDelete(id) {
-    const deleteInput = [...newTask];
+  // function handleChange(data, id) {
+  //   const inputValue = [...newTask];
+  //   inputValue[id] = data;
+  //   let value = inputValue;
+  //   setNewTasks(value);
+  // }
+  // function handleAdd() {
+  //   const newInput = [...newTask, []];
+  //   setNewTasks(newInput);
+  // }
+  // function handleDelete(id) {
+  //   const deleteInput = [...newTask];
 
-    deleteInput.splice(id, 1);
-    setNewTasks(deleteInput);
-  }
+  //   deleteInput.splice(id, 1);
+  //   setNewTasks(deleteInput);
+  // }
 
   return (
     <div><p>Nada</p></div>
