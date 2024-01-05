a7dd6c250ae45a0cbaa4f66cd517a8d68583a9f5
Leonardo Lopes <leolopesribeiro05@gmail.com>
2024-01-05 13:40:04 -0300

    Testando erro 3

--- src/pages/Lista/index.jsx
+++ src/pages/Lista/index.jsx
@@ -7,12 +7,12 @@ export default function List() {
   const storage = useStorage();
   const { state } = useLocation();
   //eslint-disable-next-line
-  const [task, setTask] = useState(storage.searchOneTask("teste"));
+  const [task, setTask] = useState(storage.searchOneTask(state.data.nome));
   const [newTask, setNewTasks] = useState([]);
   const [taskExisted, setTaskExisted] = useState(task.tarefas);
   const [inputEdit, setInputEdit] = useState(true);
 
-
+  
   let count = 0;
   task.tarefas.forEach((dados) => {
     if (dados.includes("%")) {
